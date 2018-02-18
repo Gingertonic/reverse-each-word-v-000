b52297000b69998243b8476346287990ef1eb055
Beth Schofield <bethmschofield@gmail.com>
2018-02-18 23:55:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(phrase)
   to_reverse = phrase.split(" ")
   reversed = String.new
   to_reverse.map do |word|
-    reversed << word.reverse
+    reversed << "#{word.reverse} "
   end
   reversed
 end
