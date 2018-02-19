f67e6a0666624b37bdac123e0e215752f17f57e2
Beth Schofield <bethmschofield@gmail.com>
2018-02-19 00:03:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,16 @@
+# def reverse_each_word(phrase)
+#   to_reverse = phrase.split(" ")
+#   reversed = String.new
+#   to_reverse.map do |word|
+#     reversed << "#{word.reverse} "
+#   end
+#   reversed[0...reversed.length-1]
+# end
+
 def reverse_each_word(phrase)
   to_reverse = phrase.split(" ")
   reversed = String.new
-  to_reverse.map do |word|
+  to_reverse.collect do |word|
     reversed << "#{word.reverse} "
   end
   reversed[0...reversed.length-1]
